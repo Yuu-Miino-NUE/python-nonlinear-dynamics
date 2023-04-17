ff63367c4ca9e6311955e018aaa495cb5c0b6470
Yuu Miino <ymiino@naruto-u.ac.jp>
2023-04-17 12:02:02 +0900

    Apply BLACK format

--- src/duffing.py
+++ src/duffing.py
@@ -9,29 +9,24 @@ import json, sys
 from pptools import pp
 from sympy import cos, pi
 
-# Definitions
-def ode_fun (t, state, params):
-    x, y = state
-    k = params['k']
-    B = params['B']
-    B0 = params['B0']
 
-    return [
-        y,
-        -k * y - x**3 + B0 + B * cos(t)
-    ]
+# Definitions
+def ode_fun(t, state, params):
+    x, y = state
+    k = params["k"]
+    B = params["B"]
+    B0 = params["B0"]
+
+    return [y, -k * y - x**3 + B0 + B * cos(t)]
+
 
 # Main part
 with open(sys.argv[1]) as f:
     data = json.load(f)
 
-y0 		= data['y0']
-params 	= data['params']
+y0 = data["y0"]
+params = data["params"]
 
-config = {
-    'param_keys': ['B', 'B0'],
-    'xrange': (-2, 2),
-    'yrange': (-2, 2)
-}
+config = {"param_keys": ["B", "B0"], "xrange": (-2, 2), "yrange": (-2, 2)}
 
-pp(ode_fun, 2*pi, y0, params, **config)
+pp(ode_fun, 2 * pi, y0, params, **config)
